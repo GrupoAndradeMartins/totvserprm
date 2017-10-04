aa7199bc227404ba7da35994e9fa383578ddd6a6
Rafael G. Winter <rafael@rafaelgontijo.com>
2017-10-04 16:34:39 -0300

    change setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from setuptools import setup
 
 setup(name='totvserprm',
-      version='1.0.4',
+      version='1.0.5',
       description='API para webservices do TOTVS ERP RM.',
       url='http://github.com/grupoandrademartins/totvserprm',
-      author='Rafael G. Winter, Túlio Cesar Martins',
-      author_email='gontijobh@gmail.com, funroll.loops@gmail.com',
+      contributors=[
+       ['Rafael G. Winter', 'rafael@rafaelgontijo.com', 'author'],
+       ['Túlio Cesar Martins', 'funroll.loops@gmail.com', 'author'],
+      ]
       license='MIT',
       packages=['totvserprm'],
       install_requires=[
