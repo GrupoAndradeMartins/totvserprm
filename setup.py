25e0ce87a9481995a11da3c8d4b1ae9e35facc18
Rafael G. Winter <rafael@rafaelgontijo.com>
2017-10-04 17:05:22 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-from setuptools import setup
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-setup(name='totvserprm',
-      version='1.0.6',
-      description='API para webservices do TOTVS ERP RM.',
-      url='http://github.com/grupoandrademartins/totvserprm',
-      authors=[
-        'Rafael G. Winter <rafael@rafaelgontijo.com>',
-        'Túlio Cesar Martins <funroll.loops@gmail.com>'
-      ],
-      license='MIT',
-      packages=['totvserprm'],
-      install_requires=[
-          'dicttoxml',
-          'lxml',
-          'requests',
-          'zeep'
-      ],
-      keywords='totvs webservice erp rm soap',
-      zip_safe=False)
+setup(
+    name='totvserprm',
+    version='1.0.7',
+    description='API para webservices do TOTVS ERP RM',
+    url='https://github.com/grupoandrademartins/totvserprm',
+    author='Rafael G. Winter, Túlio Cesar Martins',
+    author_email='totvs-erp-rm-python@googlegroups.com',
+    license='MIT',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=[
+      'dicttoxml',
+      'lxml',
+      'requests',
+      'zeep'
+    ],
+    keywords='totvs webservice erp rm soap api',
+    long_description=long_description
+)
