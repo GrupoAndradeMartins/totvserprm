0a78a90da60fef6f7e6fbe8ff16f4e20aca847c9
Rafael G. Winter <rafael@rafaelgontijo.com>
2017-10-06 15:13:17 -0300

    improve field names

--- totvserprm/financial.py
+++ totvserprm/financial.py
@@ -5,6 +5,7 @@ from baseapi import BaseApi
 class Client(BaseApi):
     dataservername = 'FinCFODataBR'
     def create(self,**kwargs):
+        # codigo de coligada para o contexto, diferente do dataset
         codcoligada_contexto = kwargs.get('codcoligada_contexto')
         if not codcoligada_contexto:
             codcoligada_contexto =  kwargs.get('codcoligada')
@@ -12,11 +13,13 @@ class Client(BaseApi):
         return super(Client, self).create({
             'NewDataSet': {
                 'FCFO': {
+                    'ATIVO': kwargs.get('ativo'),
+                    # enviar -1 para que sejá criado de forma incremental
+                    'CODCFO': -1,
+                    'IDCFO': -1,
                     'CODEXTERNO': kwargs.get('codexterno'),
                     'CODCOLIGADA': kwargs.get('codcoligada'),
-                    'CODCFO': -1,
-                    'CATEGORIA': kwargs.get('categoria'),
-                    'CGCCFO': kwargs.get('cpf'),
+                    'CGCCFO': kwargs.get('cpf_cnpj'),
                     'TIPORUA': kwargs.get('tipo_rua'),
                     'TIPOBAIRRO': kwargs.get('tipo_bairro'),
                     'BAIRRO': kwargs.get('bairro'),
@@ -24,14 +27,12 @@ class Client(BaseApi):
                     'NUMERO': kwargs.get('numero'),
                     'ESTADO': kwargs.get('estado'),
                     'CODMUNICIPIO': kwargs.get('codigo_municipio'),
-                    'PAIS': 1,
+                    'PAIS': kwargs.get('cod_pais'),
                     'DTNASCIMENTO': '{:%Y-%m-%d}T03:00:00.000'.format(kwargs.get('data_nascimento')),
                     'NOME': kwargs.get('nome'),
                     'NOMEFANTASIA': kwargs.get('nome'),
                     'PAGREC': kwargs.get('classificacao'),
-                    'ATIVO': True,
-                    'PESSOAFISOUJUR': 'f',
-                    'IDCFO': -1
+                    'PESSOAFISOUJUR': kwargs.get('categoria'),
                 }
             }
         }, 'CODCOLIGADA={}'.format(codcoligada_contexto))
