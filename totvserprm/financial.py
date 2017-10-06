8c43465ab561b8f3bfdd2fb0d6b80784bd365901
Rafael G. Winter <rafael@rafaelgontijo.com>
2017-10-06 13:44:09 -0300

    change a parameters requeriments from client

--- totvserprm/financial.py
+++ totvserprm/financial.py
@@ -8,25 +8,27 @@ class Client:
     def __init__(self, server, username, password):
         self.service = create_service(server, username, password)
 
-    def create(self, codexterno, codcoligada, codcfo, data_nascimento, idcfo, nome, pagrec):
+    def create(self, codexterno, codcoligada, data_nascimento, nome, pagrec, codcoligada_contexto=None):
+        if not codcoligada_contexto:
+            codcoligada_contexto = codcoligada
         client_xml = dicttoxml({
             'NewDataSet': {
                 'FCFO': {
                     'CODEXTERNO': codexterno,
                     'CODCOLIGADA': codcoligada,
-                    'CODCFO': codcfo,
+                    'CODCFO': -1,
                     'DTNASCIMENTO': '{:%Y-%m-%d}T03:00:00.000',
                     'NOME': nome,
                     'NOMEFANTASIA': nome,
                     'PAGREC': pagrec,
                     'ATIVO': True,
                     'PESSOAFISOUJUR': 'f',
-                    'IDCFO': idcfo
+                    'IDCFO': -1
                 }
             }
         }, attr_type=False)
 
-        return self.service.SaveRecord(DataServerName='FinCFODataBR', XML=client_xml, Contexto='')
+        return self.service.SaveRecord(DataServerName='FinCFODataBR', XML=client_xml, Contexto='CODCOLIGADA={}'.format(codcoligada_contexto))
 
     def get(self, codcoligada, codcfo):
         primary_key = '{};{}'.format(codcoligada, codcfo)
