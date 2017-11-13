2709bc48ba50185bb1ce92d93a83f5bdb6db5f1c
Rafael G. Winter <rafael@rafaelgontijo.com>
2017-11-13 09:47:26 -0200

    change version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='totvserprm',
-    version='1.8.2',
+    version='1.8.3',
     description='API para webservices do TOTVS ERP RM',
     url='https://github.com/grupoandrademartins/totvserprm',
     author='Rafael G. Winter, Túlio Cesar Martins',
