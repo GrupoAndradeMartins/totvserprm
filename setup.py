566dc627ac119ad060f17ea721504523a39f94aa
Túlio Cesar Martins Pereira <funroll.loops@gmail.com>
2018-01-13 18:08:42 -0200

    Alteração de versão

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='totvserprmgam',
-    version='1.0.2',
+    version='1.0.3',
     description='API para webservices do TOTVS ERP RM',
     url='https://github.com/grupoandrademartins/totvserprm',
     author='Rafael G. Winter, Túlio Cesar Martins',
