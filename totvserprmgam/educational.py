05384ce2e0909f2f8e0dd3c8fbca824010c5c378
Rafael Gontijo Winter <rafael@rafaelgontijo.com>
2018-03-08 11:29:35 -0300

    Removing coding comment

--- totvserprmgam/educational.py
+++ totvserprmgam/educational.py
@@ -46,14 +46,6 @@ class Student(BaseApi):
                         'PLANOPGTO': kwargs.get('planopagamento'),
                         'OBSFINANCEIRO': kwargs.get('obsfinanceiro')
                     }
-                    # 'SHabilitacaoAluno': {
-                    #     'CODCOLIGADA': kwargs.get('codcoligada'),
-                    #     'IDHABILITACAOFILIAL': 383,
-                    #     'RA': -1,
-                    #     'CODCURSO': kwargs.get('codcurso'),
-                    #     'CODHABILITACAO': 1,
-                    #     'CODGRADE': 1
-                    # }
                 }
             }, 'CODCOLIGADA={}'.format(kwargs.get('codcoligada')))
         else:
@@ -74,13 +66,5 @@ class Student(BaseApi):
                         'PLANOPGTO': kwargs.get('planopagamento'),
                         'OBSFINANCEIRO': kwargs.get('obsfinanceiro')
                     },
-                    # 'SHabilitacaoAluno': {
-                    #     'CODCOLIGADA': kwargs.get('codcoligada'),
-                    #     'IDHABILITACAOFILIAL': 383,
-                    #     'RA': -1,
-                    #     'CODCURSO': kwargs.get('codcurso'),
-                    #     'CODHABILITACAO': 1,
-                    #     'CODGRADE': 1
-                    # }
                 }
             }, 'CODCOLIGADA={}'.format(kwargs.get('codcoligada')))
